1fbfd84a73cc8c515f89c49f9bef746f18c30415
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2017-08-05 16:28:45 +0530

    odd

---
+++ oe.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#includeconio.h>
+void main()
+{
+int n;
+printf("enter the no:");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the no is even");
+}
+else
+{
+printf("the no is odd");
+}
+getch();
+}
