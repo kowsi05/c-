62fb9f62c2626f8eff50b8cc775298fd6943612c
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2017-08-08 16:41:29 +0530

    gr.c

---
+++ gr.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the nos:");
+scanf("%d%d%d",&a,&b,&c)
+if((a>b)||(a>c))
+{
+printf("a is greater:");
+}
+else if(b>c)
+{
+printf("b is greater:");
+}
+else
+printf("c is greater:");
+}
