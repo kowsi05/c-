086697946959d73461f496d2702e20de529c8d71
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2017-08-08 16:18:35 +0530

    alp.c

---
+++ alp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s;
+printf("Enter the character:");
+scanf("%c",&s);
+if(s>='a'&&s<='z')||(s>='A'&&s<='Z')
+{
+printf("entered character is an alphabet");
+}
+else
+printf("it is not an alphabet:");
+getch()
+}
