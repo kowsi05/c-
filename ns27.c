acef48299783341f827fa44b8f438479e5ebf4bd
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2018-02-02 16:05:45 +0530

    mistake

---
+++ ns27.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("\nEnter the input");
+	scanf("%d",&n);
+	if(n>=0||n<0)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
