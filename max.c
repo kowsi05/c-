4fdf10fdb86d0d70db98dde6cd2052b8dc673375
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2018-02-09 16:39:52 +0530

    Update max.c

--- max.c
+++ max.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 int main()
 {
- int a[20],max=a[0],i;
- printf("\n enter the ten numbers:");
- for(i=0;i<10;i++)
- {
- scanf("%d",&a[i]);
- }
- for(i=0;i<10;i++)
- {
- 	if(a[0]>a[i])
- 	max=a[i];
- }
- printf("the maximum element is %d",max);
+  int arr[10],max=arr[0],i;
+  printf("\n enter the 10 elements");
+  for(i=0;i<10;i++)
+  {
+      scanf("%d",&arr[i]);
+  }
+  for(i=1;i<10;i++)
+  {
+      if(arr[i]>max)
+      {
+          max=arr[i];
+      }
+    }
+ printf("%d",max);
  return 0;
 }
