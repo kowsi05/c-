4c8abee5331dc0d61eb0b50528a5db3228bd7e26
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2018-02-11 20:37:03 +0530

    Update 41.c

--- 41.c
+++ 41.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-	int i,n;
+	int i,k;
 	char a[100];
-	printf("\n enter the no of times:");
-	scanf("%d",&n);
 	printf("\n enter the string:");
 	scanf("%s",&a);
-	for(i=0;i<n;i++)
+	printf("\n enter the no of times:");
+	scanf("%d",&k);
+	for(i=0;i<k;i++)
 	{
 		printf("\n %s",a);
 	}
