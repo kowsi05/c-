6de9eaa45fe116cf7129875211778d5d021fcecc
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2018-02-11 21:26:41 +0530

    no in range

---
+++ 44.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("\n enter a number:");
+	scanf("%d",&n);
+	if(n>=1&&n<=10)
+	{
+		printf("\nyes");
+		
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
