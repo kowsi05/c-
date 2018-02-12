fa0cb46cb18742eae4febf98026965c8addd55e3
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2018-02-12 15:47:54 +0530

    sum

---
+++ 49.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n1,n2,sum;
+printf("\n enter the two numbers:");
+scanf("%d%d",&n1,&n2);
+sum=n1+n2;
+printf("\n the sum is:%d",sum);
+getch();
+return 0;
+}
