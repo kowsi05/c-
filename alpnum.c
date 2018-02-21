6446c2bc6b2639e9860304b56432f90982529aa2
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2018-02-21 16:13:11 +0530

    alphabet

---
+++ alpnum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a[100];
+printf("\n enter the input:");
+scanf("%s",a);
+if((a==(65-90||97-122))&&(a==0-9))
+   {
+
+       printf("\n yes");
+   }
+   else
+    {
+    printf("\n no");
+    }
+   getch();
+   return 0;
+}
