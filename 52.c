666b867eb61484baf3fb451fad96072eaaadfb20
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2018-02-23 16:28:29 +0530

    print numbers

---
+++ 52.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<coio.h>
+int main()
+{
+	int n;
+	printf("\n enter the number between 1 to 10:");
+	scanf("%d",&n);
+	if(n==1)
+	{
+		printf("\n one");
+	}
+	else if(n==2)
+	{
+		printf("\ntwo");
+	}
+	else if(n==3)
+	{
+		printf("\n three");
+	}
+	else if(n==4)
+	{
+		printf("\n four");
+	}
+	else if(n==5)
+	{
+		printf("\n five");
+	}
+	else if(n==6)
+	{
+		printf("\n six");
+	}
+	else if(n==7)
+	{
+		printf("\n seven");
+	}
+	else if(n==8)
+	{
+		printf("\n eight");
+	}
+	else if(n==9)
+	{
+		printf("\n nine");
+	}
+	else
+	{
+		printf("\n ten");
+	}
+	getch();
+	return 0;
+}
