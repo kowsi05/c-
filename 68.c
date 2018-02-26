27baf905507875030ccc33037b3f55c52fa1b7a1
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2018-02-26 16:35:40 +0530

    mul of 7

---
+++ 68.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("\n enter the number: ");
+scanf("%d",&n);
+if(n%7==0)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+return 0;
+}
