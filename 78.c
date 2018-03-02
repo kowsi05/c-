b7133c02313dacab57e6a5274a3aeabc9e14a734
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2018-03-02 15:52:42 +0530

    13 mul

---
+++ 78.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("\n enter the number:");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+return 0;
+}
