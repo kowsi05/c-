869538d0a984ed6a8ed92b9e5a3903e7057d221b
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2018-03-09 15:46:07 +0530

    float number

---
+++ 82.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+float l,b,area;
+printf("\n enter the length and breadth:");
+scanf("%f%f",&l,&b);
+area=l*b;
+printf("\n%0.5f",area);
+return 0;
+}
