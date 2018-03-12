4f66bb6f9e713169aca86286a44f24717636946b
kowsi05 <30748406+kowsi05@users.noreply.github.com>
2018-03-12 16:49:23 +0530

    cel to kelvin

---
+++ 93.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int cel,kel;
+printf("\n enter the input in celsius");
+scanf("%d",&cel);
+kel=cel+273.15;
+printf("%d",kel);
+getch();
+return 0;
+}
